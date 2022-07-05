448ba213141b80f879faf70bc1ee4c5a8c23328b
Pierre-Antoine Champin <pierre-antoine@w3.org>
2022-07-05 14:28:16 +0200

    still mono-thread, but with a Mutex
    The Mutex is of course not required here,
    but at least we see that we *can* wrap our map in a Mutex,
    and that it works.
    
    Unfortunately, we still have an error when we try to make
    it multi-thread again.

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,18 @@ use std::collections::HashMap;
 use std::io;
 use std::io::Write;
 use std::net::{TcpListener, TcpStream};
+use std::sync::Mutex;
 use std::time::Duration;
 use std::thread::{sleep, spawn};
 
 fn main() -> Result<(), io::Error> {
-    let mut map = HashMap::new();
+    let map = Mutex::new(HashMap::new());
     let listener = TcpListener::bind("127.0.0.1:12345")?;
     for stream in listener.incoming() {
         let stream = stream?;
         println!("{:?}", stream);
         //spawn(||
-            if let Err(e) = handle(stream, &mut map) {
+            if let Err(e) = handle(stream, &map) {
                 eprintln!("=== {}", e);
             }
         //);
@@ -20,10 +21,11 @@ fn main() -> Result<(), io::Error> {
     Ok(())
 }
 
-fn handle(mut stream: TcpStream, map: &mut HashMap<usize, u16>) -> Result<(), io::Error> {
+fn handle(mut stream: TcpStream, map: &Mutex<HashMap<usize, u16>>) -> Result<(), io::Error> {
     let port = stream.peer_addr()?.port();
     for _ in 0..5 {
         sleep(Duration::new(1, 0));
+        let mut map = map.lock().unwrap();
         let mid = map.len();
         map.insert(mid, port);
         let msg = format!("hello {} ({})\n", port, mid);
