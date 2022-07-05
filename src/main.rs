1ec3cd448d600e54d737ea1bb23ad98c64e15d7c
Pierre-Antoine Champin <pierre-antoine@w3.org>
2022-07-05 14:58:50 +0200

    async server with shared state, no concurency

--- src/main.rs
+++ src/main.rs
@@ -9,25 +9,28 @@ fn main() -> Result<(), io::Error> {
 }
 
 async fn run() -> Result<(), io::Error> {
+    let mut state = vec![];
     let listener = TcpListener::bind("127.0.0.1:12345").await?;
     let mut incoming = listener.incoming();
     while let Some(stream) = incoming.next().await {
         let stream = stream?;
         println!("{:?}", stream);
-        task::spawn(async {
-            if let Err(e) = handle(stream).await {
+        //task::spawn(async {
+            if let Err(e) = handle(stream, &mut state).await {
                 eprintln!("=== {}", e);
             }
-        });
+        //});
     }
     Ok(())
 }
 
-async fn handle(mut stream: TcpStream) -> Result<(), io::Error> {
+async fn handle(mut stream: TcpStream, state: &mut Vec<u16>) -> Result<(), io::Error> {
     let port = stream.peer_addr()?.port();
     for _ in 0..5 {
         task::sleep(Duration::new(1, 0)).await;
-        let msg = format!("hello {}\n", port);
+        let mid = state.len();
+        state.push(port);
+        let msg = format!("hello {} ({})\n", port, mid);
         stream.write(msg.as_bytes()).await?;
     }
     Ok(())
