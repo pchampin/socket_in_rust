49887c07033b870f42fe11da299086b9b37bc45c
Pierre-Antoine Champin <pierre-antoine@w3.org>
2022-07-05 10:25:59 +0200

    back to single thread, with shared state

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io;
 use std::io::Write;
 use std::net::{TcpListener, TcpStream};
@@ -5,24 +6,27 @@ use std::time::Duration;
 use std::thread::{sleep, spawn};
 
 fn main() -> Result<(), io::Error> {
+    let mut map = HashMap::new();
     let listener = TcpListener::bind("127.0.0.1:12345")?;
     for stream in listener.incoming() {
         let stream = stream?;
         println!("{:?}", stream);
-        spawn(||
-            if let Err(e) = handle(stream) {
+        //spawn(||
+            if let Err(e) = handle(stream, &mut map) {
                 eprintln!("=== {}", e);
             }
-        );
+        //);
     }
     Ok(())
 }
 
-fn handle(mut stream: TcpStream) -> Result<(), io::Error> {
+fn handle(mut stream: TcpStream, map: &mut HashMap<usize, u16>) -> Result<(), io::Error> {
     let port = stream.peer_addr()?.port();
-    let msg = format!("hello {}\n", port);
     for _ in 0..5 {
         sleep(Duration::new(1, 0));
+        let mid = map.len();
+        map.insert(mid, port);
+        let msg = format!("hello {} ({})\n", port, mid);
         stream.write(msg.as_bytes())?;
     }
     Ok(())
