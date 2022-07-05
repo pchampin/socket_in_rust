3065331c52a093e5d9aff8e57ceb787063805a89
Pierre-Antoine Champin <pierre-antoine@w3.org>
2022-07-05 15:03:13 +0200

    async concurrent server with shared state

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,38 @@
 use async_std::prelude::*;
 use async_std::net::{TcpListener, TcpStream};
+use async_std::sync::Mutex;
 use async_std::task;
 use std::io;
 use std::time::Duration;
+
+lazy_static::lazy_static! {
+    static ref STATE: Mutex<Vec<u16>> = Mutex::new(vec![]);
+}
 
 fn main() -> Result<(), io::Error> {
     task::block_on(run())
 }
 
 async fn run() -> Result<(), io::Error> {
-    let mut state = vec![];
     let listener = TcpListener::bind("127.0.0.1:12345").await?;
     let mut incoming = listener.incoming();
     while let Some(stream) = incoming.next().await {
         let stream = stream?;
         println!("{:?}", stream);
-        //task::spawn(async {
-            if let Err(e) = handle(stream, &mut state).await {
+        task::spawn(async {
+            if let Err(e) = handle(stream, &STATE).await {
                 eprintln!("=== {}", e);
             }
-        //});
+        });
     }
     Ok(())
 }
 
-async fn handle(mut stream: TcpStream, state: &mut Vec<u16>) -> Result<(), io::Error> {
+async fn handle(mut stream: TcpStream, state: & Mutex<Vec<u16>>) -> Result<(), io::Error> {
     let port = stream.peer_addr()?.port();
     for _ in 0..5 {
         task::sleep(Duration::new(1, 0)).await;
+        let mut state = state.lock().await;
         let mid = state.len();
         state.push(port);
         let msg = format!("hello {} ({})\n", port, mid);
