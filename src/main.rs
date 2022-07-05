03a9f80b68c2870f5266c465f3f0a09b1a31cc79
Pierre-Antoine Champin <pierre-antoine@w3.org>
2022-07-05 14:41:47 +0200

    async version of the 'hello world' server

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-use std::net::TcpListener;
+use async_std::prelude::*;
+use async_std::net::TcpListener;
+use async_std::task;
 use std::io;
-use std::io::Write;
 
 fn main() -> Result<(), io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:12345")?;
-    for stream in listener.incoming() {
+    task::block_on(run())
+}
+
+async fn run() -> Result<(), io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:12345").await?;
+    let mut incoming = listener.incoming();
+    while let Some(stream) = incoming.next().await {
         let mut stream = stream?;
         println!("{:?}", stream);
-        stream.write("hello world\n".as_bytes())?;
+        stream.write("hello world\n".as_bytes()).await?;
     }
     Ok(())
 }
