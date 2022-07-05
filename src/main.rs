78a0c84ac6204ee68ac9f5382d80e7ac9224c7c9
Pierre-Antoine Champin <pierre-antoine@w3.org>
2022-07-05 14:49:31 +0200

    async error-handling 5×'hello world' server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use async_std::prelude::*;
-use async_std::net::TcpListener;
+use async_std::net::{TcpListener, TcpStream};
 use async_std::task;
 use std::io;
 use std::time::Duration;
@@ -12,14 +12,21 @@ async fn run() -> Result<(), io::Error> {
     let listener = TcpListener::bind("127.0.0.1:12345").await?;
     let mut incoming = listener.incoming();
     while let Some(stream) = incoming.next().await {
-        let mut stream = stream?;
+        let stream = stream?;
         println!("{:?}", stream);
-        let port = stream.peer_addr()?.port();
-        for _ in 0..5 {
-            task::sleep(Duration::new(1, 0)).await;
-            let msg = format!("hello {}\n", port);
-            stream.write(msg.as_bytes()).await?;
+        if let Err(e) = handle(stream).await {
+            eprintln!("=== {}", e);
         }
+    }
+    Ok(())
+}
+
+async fn handle(mut stream: TcpStream) -> Result<(), io::Error> {
+    let port = stream.peer_addr()?.port();
+    for _ in 0..5 {
+        task::sleep(Duration::new(1, 0)).await;
+        let msg = format!("hello {}\n", port);
+        stream.write(msg.as_bytes()).await?;
     }
     Ok(())
 }
