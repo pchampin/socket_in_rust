654e316e22056cd2f61a73cd29e7b6551a3206f2
Pierre-Antoine Champin <pierre-antoine@w3.org>
2022-07-05 10:08:27 +0200

    move stream handling to a separate function

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
 use std::io;
 use std::io::Write;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 use std::time::Duration;
 use std::thread::sleep;
 
 fn main() -> Result<(), io::Error> {
     let listener = TcpListener::bind("127.0.0.1:12345")?;
     for stream in listener.incoming() {
-        let mut stream = stream?;
+        let stream = stream?;
         println!("{:?}", stream);
-        let port = stream.peer_addr()?.port();
-        let msg = format!("hello {}\n", port);
-        for _ in 0..5 {
-            sleep(Duration::new(1, 0));
-            stream.write(msg.as_bytes())?;
+        handle(stream)?;
+    }
+    Ok(())
+}
+
+fn handle(mut stream: TcpStream) -> Result<(), io::Error> {
+    let port = stream.peer_addr()?.port();
+    let msg = format!("hello {}\n", port);
+    for _ in 0..5 {
+        sleep(Duration::new(1, 0));
+        if let Err(e) = stream.write(msg.as_bytes()) {
+            eprintln!("=== {}", e);
+            break;
         }
     }
     Ok(())
