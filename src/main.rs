24d65fe88a866e3bc01a241156328e64e9865884
Pierre-Antoine Champin <pierre-antoine@w3.org>
2022-07-05 09:38:18 +0200

    'hello world' server

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::net::TcpListener;
 use std::io;
+use std::io::Write;
 
 fn main() -> Result<(), io::Error> {
     let listener = TcpListener::bind("127.0.0.1:12345")?;
     for stream in listener.incoming() {
+        let mut stream = stream?;
         println!("{:?}", stream);
+        stream.write("hello world\n".as_bytes())?;
     }
     Ok(())
 }
