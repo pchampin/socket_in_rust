52a286a325251ffe0bb92ffc90f7a7d1b3ee218a
Pierre-Antoine Champin <pierre-antoine@w3.org>
2022-07-05 09:43:40 +0200

    5×'hello world' server

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-use std::net::TcpListener;
 use std::io;
 use std::io::Write;
+use std::net::TcpListener;
+use std::time::Duration;
+use std::thread::sleep;
 
 fn main() -> Result<(), io::Error> {
     let listener = TcpListener::bind("127.0.0.1:12345")?;
     for stream in listener.incoming() {
         let mut stream = stream?;
         println!("{:?}", stream);
-        stream.write("hello world\n".as_bytes())?;
+        let port = stream.peer_addr()?.port();
+        let msg = format!("hello {}\n", port);
+        for _ in 0..5 {
+            sleep(Duration::new(1, 0));
+            stream.write(msg.as_bytes())?;
+        }
     }
     Ok(())
 }
